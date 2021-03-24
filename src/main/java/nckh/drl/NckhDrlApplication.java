154c6ba953e6327c1bbebe932ba6939e84d24776
nguyenhuuvong-nhv <71600841+nguyenhuuvong-nhv@users.noreply.github.com>
2021-03-24 22:20:27 +0700

    first commit

---
+++ src/main/java/nckh/drl/NckhDrlApplication.java
@@ -0,0 +1,13 @@
+package nckh.drl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NckhDrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NckhDrlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nckh/drl/NckhDrlApplicationTests.java
@@ -0,0 +1,13 @@
+package nckh.drl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NckhDrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
